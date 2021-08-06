582997b17076c4a9cabb8394981b8274e77ddfc3
Ryan <>
2021-08-06 15:05:38 -0400

    cpu

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2352,7 +2352,7 @@ class MaskRCNN(object):
         if os.name == 'nt':
             workers = 0
         else:
-            workers = multiprocessing.cpu_count()
+            workers = 0
 
         self.keras_model.fit(
             train_generator,
