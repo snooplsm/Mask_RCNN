bccd221865e3350f9e7aefb444d034689d4f829a
The Gravener <ryan@wmwm.us>
2021-08-10 08:58:12 -0400

    work is done

--- samples/balloon/balloon2.py
+++ samples/balloon/balloon2.py
@@ -328,5 +328,6 @@ if __name__ == '__main__':
                                 video_path=args.video)
 
     converter = tf.lite.TFLiteConverter.from_keras_model(model)
+    tflite_model = converter.convert()
     with open('model.tflite', 'wb') as f:
         f.write(tflite_model)
